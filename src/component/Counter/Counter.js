69705c66ea2b088a75d96d72b92597ceb9e6be45
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-05-10 13:49:47 +0430

    if you click button add counter the number is add one number and if you click lower button the number is one lower

--- src/component/Counter/Counter.js
+++ src/component/Counter/Counter.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import "./Counter.css";
 export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Count: 0,
+    };
+  }
+  addCount() {
+    this.setState((prevState) => {
+      return { Count: prevState.Count + 1 };
+    });
+  }
+  lowerCount() {
+    this.setState((prevState) => {
+      return { Count: prevState.Count - 1 };
+    });
+  }
+
   render() {
     return (
       <div>
         <section id="main">
           <div className="container">
             <h2 id="title">Counter</h2>
-            <h3 id="counter">0</h3>
+            <h3 id="counter">{this.state.Count}</h3>
           </div>
           <div className="btn-container">
-            <button id="add">Add Count</button>
-            <button id="lower">Lower Count</button>
+            <button id="add" onClick={this.addCount.bind(this)}>
+              Add Count
+            </button>
+            <button id="lower" onClick={this.lowerCount.bind(this)}>
+              Lower Count
+            </button>
           </div>
         </section>
       </div>
